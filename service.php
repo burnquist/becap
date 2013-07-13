109d35204623a4acc491cb4f951276420359d270
Bambang Priyatna <burnquist73@yahoo.com>
2013-07-14 00:42:35 +0700

    upload

---
+++ service.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+
+		<title>Heat Transfer Solution - Becap | Benua Cakra Petrolindo , PT</title>
+		<meta name="description" content="" />
+		<meta name="author" content="Austin" />
+
+		<!--Grab the Stylesheets-->
+		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400' rel='stylesheet' type='text/css'>
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+		<link href="css/flexslider.css" rel="stylesheet" type="text/css">
+<!--[if IE]>
+	<link rel="stylesheet" type="text/css" href="css/ie-style.css" />
+<![endif]-->
+		
+
+		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
+
+		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+		<link rel="shortcut icon" href="/favicon.ico" />
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+	</head>
+
+	<body>
+		<div class="container">
+		<header class="sixteen columns alpha omega">
+				<a href="index.php"><img class="" src="img/logo-becap.png" alt="Becap"></a>
+				<nav class="main-nav sixteen columns">
+				
+					<ul class=" columns alpha">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="product.php">Product</a></li>
+						<li><a href="service.php">Service</a></li>
+						<li><a href="ourclient.php">Ourclient</a></li>
+						<li><a href="achievements.php">Achievements</a></li>
+						<li><a href="contactus.php">Contact Us</a></li>
+						<li><a href="carrier.php">Carrier</a></li>
+					</ul>
+					<div class="social three columns omega">
+						<a href="https://www.facebook.com/pages/PT-Benua-Cakra-Petrolindo/107661925931172"></a>
+						<a href="http://www.twitter.com"></a>
+					</div>
+				</nav>
+				
+			</header>
+	     	<div class="sixteen columns">
+		      	<div class="h-border">
+					<div class="heading">
+						<h1>Service</h1>
+					</div><!--Close "Heading" Div-->
+				</div><!--Close H-border Div-->
+			</div>
+			<section class="row contact">
+				<p style="margin-left :15px;text-align:justify;text-justify:inter-word;font-size:100%;background-color:#;"><b>Heat Tranfer Solutions.</b></p> </br>
+
+<p style="margin-left :15px;text-align:justify;text-justify:inter-word;font-size:100%;background-color:#;">PT. Benua Cakra Petrolindo or (BECAP) is, heat transfer company based solutions are highly experienced, serving the Oil and Gas, Petrolium Test Instrument, Soot Blower, Combust on Equipment, Gas Analyzer, Plant Performance & Effeciensi Assessment, Installation Commisioning & Training Calibration & Troubleshooting, Overhaul and Maintenance in Industrial sector.</p> 
+ <p style="margin-left :15px;text-align:justify;text-justify:inter-word;font-size:100%;background-color:#;">Committed to providing our clients with design solutions are the most effective heat transfer and remain qualified, competitive providers in terms of supply, manufacturing, maintenance, cleanup and site services and Spare Part Supply.
+
+BECAP will provide the following services for all your Heat transfer in accordance with the requirements:</p></br>
+<p style="text-align:justify;
+text-justify:inter-word;font-size:100%;background-color:#;"><ul>
+    <li> * Design</li>
+    <li> * Manufacture</li>
+    <li> * Maintenance</li>
+    <li> * Cleaning and Site Service</li>
+    <li> * Spares<li>
+</ul>
+</p></br>
+
+<p style="margin-left :15px;text-align:justify;text-justify:inter-word;font-size:100%;background-color:#;">Our in-house expertise Enables us to address technically sophisticated projects and, through our partnerships with global leaders, more technically sophisticated equipment and supply.
+
+Our Maintenance Workshop has an enviable reputation for the service, rebuild and repair.
+
+We have the engineering depth for complex heat transfer solutions in both Thermal and Mechanical Design disciplines and code conformance requirements with design capabilities</p>
+</p></br></section>	
+			<div class="clear"></div>
+
+			<footer class="row">
+				<div class="eight columns omega">
+					<p>Copyright&copy;2013 PT. Benua Cakra Petrolindo.</p>
+				</div>
+					<nav class="eight columns alpha">
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="contactus.php">Contact Us</a></li>
+							
+						</ul>
+					</nav>
+			</footer>
+		</div><!--Close Container Div-->
+		
+		<!--Grab JS Files-->
+		<script type="text/javascript" src="js/jquery.js" ></script>
+		<script type="text/javascript" src="js/jquery.flexslider.js" ></script>
+		<script type="text/javascript">
+			// Can also be used with $(document).ready()
+			$(window).load(function() {
+			  $('.flexslider').flexslider({
+			    animation: "slide"
+			  });
+			});
+		</script>
+	</body>
+</html>
