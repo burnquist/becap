ea6c7f2f5dd238b3abc9ab8e6d8a40a9a201efb1
Bambang Priyatna <burnquist73@yahoo.com>
2013-08-07 20:51:44 +0700

    update

---
+++ php/ourclient-edit.php
@@ -0,0 +1,388 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Our Client | Heat Transfer Solution</title>
+<script type="text/javascript" src="js/jquery.min.js"></script>	
+<script type="text/javascript" src="js/scroll.js"></script>
+<link rel="stylesheet" href="css/img-slider.css">
+<style>
+   /* Begin Navigation Bar Styling */
+   #nav {
+      width: 100%;
+      float: left;
+      margin: -50px 0 35px 300px;
+      padding: 0px;
+      list-style: none;
+      background-color: #fff;
+      border-bottom: 7px solid red; 
+      border-top: 0px solid #ccc; 
+	  }
+   #nav li {
+      float: left;
+	  indent:3em;		}
+   #nav li a {
+      display: block;
+      padding: 8px 15px;
+      text-decoration: none;
+      font-weight: bold;
+      color: red;
+      border-right: 1px solid #ccc; }
+   #nav li a:hover {
+      color: white;
+      background-color: red; }
+   /* End navigation bar styling. */
+   
+   /* This is just styling for this specific page. */
+   body {
+      background-color: #fff;position: absolute; 
+      font: small/1.3 Arial, Helvetica, sans-serif; }
+   #wrap {
+      width: 645px;
+      margin: 0 0 0 18em;
+      background-color: #fff; }
+   h1 {
+      font-size: 1.5em;
+      color: #fff;
+      background-color: #069;
+      margin: 6em 0 -6em 0; }
+	
+
+#divLine
+{
+	margin-left: 275px;
+    margin-right: auto;
+    margin-top: 3px;
+    margin-bottom: auto;
+	font-family: Verdana,sans;
+    font-size: 18px;
+	color: #1C2A47;
+	width:  906px;
+	border-top: solid 3px #B3B3B3;
+	font-weight: strong;
+}
+.divfooter
+{
+	margin-left: 22em;
+    margin-right: auto;
+    margin-top: 10em;
+    margin-bottom: auto;
+	font-family: Tahoma, Verdana, Sans;
+    font-size: 12px;
+	color: #1C2A47;
+	width: 920px;
+	border-top: solid 1px #B3B3B3;
+	font-weight: bold;
+}
+
+ #content {
+	margin: 40px 0 0 240px;
+	width:1000px;
+	padding: 0 50px 50px;
+	background-color: #fff;
+	}
+	
+ #content_1 {
+    margin: 320px 10px 0px -250px;
+	width:1000px;
+	padding: 0 50px 50px;
+	background-color: #fff;
+	border-bottom:2px  solid #B3B3B3; 
+    border-top: 2px solid #B3B3B3; 
+	border-left:2px solid #B3B3B3;
+	border-right:2px solid #B3B3B3;
+	height: 270;
+	  }
+	  
+#container-right {
+      margin-left:37em;
+	  margin-top:-390px;
+	  font-size: 17px;
+	  font-weight: strong;
+	  font-family: calibri, Verdana, Sans;
+	  }
+#con-box {
+	padding:0px;
+	margin:5px 0 0 -35px;
+	width:200px;
+	
+	border-bottom:2px  solid #B3B3B3; 
+    border-top: 2px solid #B3B3B3; 
+	border-left:2px solid #B3B3B3;
+	border-right:2px solid #B3B3B3;
+	
+}	
+#con-box1 {
+	padding:0px;
+	margin:-313px 0 0 190px;
+	width:200px;
+	border-bottom:2px  solid #B3B3B3; 
+    border-top: 2px solid #B3B3B3; 
+	border-left:2px solid #B3B3B3;
+	border-right:2px solid #B3B3B3;
+	
+}
+#con-box2 {
+	padding:0px;
+	margin:-313px 0 0 415px;
+	width:200px;
+	border-bottom:2px  solid #B3B3B3; 
+    border-top: 2px solid #B3B3B3; 
+	border-left:2px solid #B3B3B3;
+	border-right:2px solid #B3B3B3;
+	
+}
+#con-box3 {
+	padding:0px;
+	margin:-313px 0 0 645px;
+	width:200px;
+	border-bottom:2px  solid #B3B3B3; 
+    border-top: 2px solid #B3B3B3; 
+	border-left:2px solid #B3B3B3;
+	border-right:2px solid #B3B3B3;
+	
+}
+
+#con-box4 {
+	padding:0px;
+	margin:40px 0 0 -35px;
+	width:200px;
+	border-bottom:2px  solid #B3B3B3; 
+    border-top: 2px solid #B3B3B3; 
+	border-left:2px solid #B3B3B3;
+	border-right:2px solid #B3B3B3;
+	
+}	
+#con-box5 {
+	padding:0px;
+	margin:-313px 0 0 190px;
+	width:200px;
+	border-bottom:2px  solid #B3B3B3; 
+    border-top: 2px solid #B3B3B3; 
+	border-left:2px solid #B3B3B3;
+	border-right:2px solid #B3B3B3;
+	
+}
+#con-box6 {
+	padding:0px;
+	margin:-313px 0 0 415px;
+	width:200px;
+	border-bottom:2px  solid #B3B3B3; 
+    border-top: 2px solid #B3B3B3; 
+	border-left:2px solid #B3B3B3;
+	border-right:2px solid #B3B3B3;
+	
+}
+#con-box7 {
+	padding:0px;
+	margin:-313px 0 0 645px;
+	width:200px;
+	border-bottom:2px  solid #B3B3B3; 
+    border-top: 2px solid #B3B3B3; 
+	border-left:2px solid #B3B3B3;
+	border-right:2px solid #B3B3B3;
+	
+}
+
+
+
+
+
+#box
+{
+margin:-2px 0 0 -4px;
+width:268px;
+-moz-column-count:4; /* Firefox */
+-webkit-column-count:4; /* Safari and Chrome */
+	column-count:4;
+	border-bottom:2px  solid #B3B3B3; 
+    border-top: 2px solid #B3B3B3; 
+	border-left:2px solid #B3B3B3;
+	border-right:2px solid #B3B3B3;
+}
+#box2
+{
+margin:-13px 0 0 16em;
+width:268px;
+-moz-column-count:1; /* Firefox */
+-webkit-column-count:1; /* Safari and Chrome */
+	column-count:1;
+	border-bottom:2px  solid #B3B3B3; 
+    border-top: 2px solid #B3B3B3; 
+	border-left:2px solid #B3B3B3;
+	border-right:2px solid #B3B3B3;
+}
+
+	  .container-right-2 {
+      margin:11px 0 0 20px;
+	  font-size: 17px;
+	  font-weight: strong;
+	  font-family: calibri, Verdana, Sans;
+	  }	  
+	    
+	  
+	  .mrg{
+	  margin:2px
+	  }
+	.mrg2{
+	   margin:-280px 0 0 200px;
+	  }	
+	  
+	  
+	   
+	
+	
+	a:link {
+	
+	color:#0d0c0c;
+
+	}
+
+	a:visited {
+	
+	color:#615b5b;
+	
+	}
+	a:hover {
+	color: #0d0c0c;
+	
+	}
+	
+	div.img
+{
+  margin: -5px 5px  0px 5px;
+  border: 2px  solid #B3B3B3;
+  height: 270;
+  width: 200;
+  float: left;
+  text-align: center;
+}	
+
+	div.img2
+{
+  margin: 50px 270px  0px -350px;
+  border: 2px  solid #B3B3B3;
+  height: 120;
+  width: 380;
+  float: left;
+  text-align: center;
+}
+div.img img
+{
+  display: inline;
+  margin: 3px;
+  border: 1px solid #ffffff;
+}
+div.img a:hover img {border: 1px solid #0000ff;}
+div.desc
+{
+  text-align: center;
+  font-weight: normal;
+  width: 120px;
+  margin: 2px;
+}
+
+	
+	
+	
+	
+</style>
+</head>
+
+<body>
+<div id="wrap">
+   
+   <img style="margin:0em 0em -2px 3em;" src="logo/logo-becap.png" alt="Becap"></a>
+   <!-- Here's all it takes to make this navigation bar. -->
+   <ul id="nav">
+     <li><a style="background-color:red;color:white;" href="index.php">Home</a></li>
+		<li><a  href="">Products</a></li>
+        <li><a  href="">Services</a>	</li>
+		<li><a  href="ourclient.php">Our Clients</a></li>
+		<li><a  href="">Achievements</a></li>
+		<li><a  href="">Contact Us</a></li>
+        <li><a  href="">Career</a></li>	
+   </ul>
+   
+   
+  	</div>
+	<div id="divLine">
+	
+	</div>
+	<p style="color:red;font-size:120%;float:left;margin-left:295px; font-weight: strong;"> <b>OIL & REFENERY </b></p>
+	<p style="color:red;font-size:120%;float:left;margin-left:91px; font-weight: strong;"> <b>GAS PRODUCERS </b></p>
+	<p style="color:red;font-size:120%;float:left;margin-left:85px; font-weight: strong;"> <b> PETROCHEMICAL </b></p>
+	<p style="color:red;font-size:120%;float:left;margin-left:90px; font-weight: strong;"> <b>PULP AND PAPERS </b></p>
+	
+	<div id="content">
+	
+	</br>
+	
+	<div class="img">
+	<img src="logo/ourclient/oil-gas.jpg" alt="ourclient" width="200" height="270"></a>
+
+	</div>
+	<div class="img">
+	<img src="logo/ourclient/cement.jpg" alt="ourclient" width="200" height="270"></a>
+ 
+	</div>
+	<div class="img">
+	<img src="logo/ourclient/power.jpg" alt="ourclient" width="200" height="270"></a>
+
+	</div>
+	<div class="img">
+	<img src="logo/ourclient/paper.jpg" alt="ourclient" width="200" height="270"></a>
+
+	</div>
+	
+	</br>
+	<p style="color:red;font-size:120%;float:left;margin-left:5px; font-weight: strong;"> <b>OIL & REFENERY </b></p>
+	<p style="color:red;font-size:120%;float:left;margin-left:91px; font-weight: strong;"> <b>GAS PRODUCERS </b></p>
+	<p style="color:red;font-size:120%;float:left;margin-left:85px; font-weight: strong;"> <b> PETROCHEMICAL </b></p>
+	<p style="color:red;font-size:120%;float:left;margin-left:90px; font-weight: strong;"> <b>PULP AND PAPERS </b></p>
+	<div class="img">
+	<img src="logo/ourclient/palm.jpg" alt="ourclient" width="200" height="270"></a>
+	</div>
+
+	<div class="img">
+	<img src="logo/ourclient/cement.jpg" alt="ourclient" width="200" height="270"></a>
+	</div>
+	
+	<div class="img">
+	<img src="logo/ourclient/power.jpg" alt="ourclient" width="200" height="270"></a>
+	</div>
+
+	<div class="img">
+	<img src="logo/ourclient/Fertilizer.jpg" alt="ourclient" width="200" height="270"></a>
+	</div>
+			
+	</br>
+	<p style="color:red;font-size:120%;float:left;margin-left:5px; font-weight: strong;"> <b>OIL & REFENERY </b></p>
+	<p style="color:red;font-size:120%;float:left;margin-left:180px; font-weight: strong;"> <b>GAS PRODUCERS </b></p>
+	</br>
+	<div class="img2">
+	<p><img style="margin:0em 0em -2px 0em;" src="logo/ourclient/food.jpg" alt="Becap"></a></p>
+	</div>
+	
+	<div class="img2">
+	<p><img style="margin:0em 0em -2px 0em;" src="logo/ourclient/lab-service.jpg" alt="Becap"></a></p>
+	</div>
+	
+	</div>
+	<!------footer-------!><!---->
+	
+	</br>
+	
+	<p style=" margin: 3em 0 0 0;float:right;font-size:95%;color #485551 text-indent:50px;"><b style="color:#077759; text-indent:50px;">PT. Benua Cakra Petrolindo</b></br> 
+	Kompleks Roxy Mas Blok D5 No. 3<br>
+	Jakarta 10150, Indonesia<br>
+	Email:becap@becap.com<br>
+	</p>
+	<p style=" margin: 3em 0 0 21em;float:left;font-size:100%;color #485551;"><img style="width:200px;"src="logo/logo-footer.jpg"  /> <br></p>
+	
+	<div class="divfooter">
+	<p style="float:left;">Copyright@2013 PT. Benua Cakra Petrolindo</p>
+	<p style="float:right;"><a style="text-decoration:none;" href="">Home</a> | <a  style="text-decoration:none;" href="">Contact Us</a></p>	<br>
+</div>
+</body>
+</html>
